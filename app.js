b9163ac479a84428c62d45a730617d2e4daef293
SANKALP PANDEY <84143581+SANKALP1011@users.noreply.github.com>
2022-12-07 22:39:45 +0530

    Mongo cluster added

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+const moongose = require("mongoose");
+require("dotenv").config({ path: require("find-config")(".env") });
+console.log(process.env.MONGO_URI);
+
+moongose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Database connected"))
+  .catch((e) => {
+    console.log(e);
+  });
 
 app.listen("3001", (err) => {
   console.log("Server is up and running");
