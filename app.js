5a2b9759ff82fe8bd0ca978d24408d3e8df629e2
SANKALP PANDEY <84143581+SANKALP1011@users.noreply.github.com>
2023-02-03 23:53:33 +0530

    fiximg swagger issues

--- app.js
+++ app.js
@@ -28,6 +28,25 @@ app.use(UserRoutes);
 app.use(AnimalRoutes);
 app.use(DocterRoutes);
 app.use(NgoRoutes);
+// app.use(function (req, res, next) {
+
+//   // Website you wish to allow to connect
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3008');
+
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+//   // Request headers you wish to allow
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+
+//   // Pass to next layer of middleware
+//   next();
+// })
+
 app.listen(process.env.PORT || "3001", (err) => {
   console.log("Server is up and running");
 });
