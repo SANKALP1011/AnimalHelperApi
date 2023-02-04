02455a77e268100adc7ea8b1768eaa0f747f1730
SANKALP PANDEY <84143581+SANKALP1011@users.noreply.github.com>
2023-02-04 23:22:04 +0530

    swagger bug resolve phase one

--- swagger.js
+++ swagger.js
@@ -15,7 +15,17 @@ const doc = {
     description:
       "This is the server of the animal welfare api made for the well being of the animals.",
   },
-  host: "localhost:3001",
+  servers: [
+    {
+      url: "https://animal-welfare-api.herokuapp.com",
+      description: "Production Server",
+    },
+    {
+      url: "http://localhost:3001",
+      description: "Testing Server",
+    },
+  ],
+  host: "https://animal-welfare-api.herokuapp.com",
   securitySchemes: {
     bearerAuth: {
       type: "http",
